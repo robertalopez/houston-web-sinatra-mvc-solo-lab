c2f84e31da0f9c491256f5c60102fe981df47e6a
robertalopez <roblop1234@yahoo.com>
2020-06-16 11:42:19 -0700

    maybe done

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,3 +1,27 @@
 class PuppiesController < ApplicationController
-   
+   get '/index' do 
+    @puppies = Puppy.all 
+    erb :index 
+   end 
+
+   get '/puppy/:id' do 
+    @puppy = Puppy.find(params[:id])
+    erb :show 
+   end 
+
+   get '/puppy/new' do 
+    erb :new_puppy 
+   end 
+
+   post '/puppies' do 
+    Puppy.create(params)
+    erb :index 
+   end 
+
+   delete '.puppies/:id' do 
+    puppy = Puppy.find(params[:id])
+    puppy.destroy
+    erb :index
+   end 
+
 end
